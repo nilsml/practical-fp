9fa732bd61a442e677f667ccadde7b34684e186b
Nils Magne Lunde <nilsml@nilsland.com>
2023-03-18 09:16:44 +0100

    🔧 Set up eslint and prettier

---
+++ src/.prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'always',
+    printWidth: 120,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'none',
+    semi: false,
+    endOfLine: 'lf',
+  }
+  
